6849d22cb1d4775cb12b0a043e4f75c4ae09cbfa
mauricio gallego <mgallego@sysgarage.com>
2024-05-07 10:16:33 -0500

    setting text on experience page

--- src/components/tab/tab.tsx
+++ src/components/tab/tab.tsx
@@ -1,21 +1,24 @@
 import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import { Each } from '../common/each/each';
 import clsx from 'clsx';
 import works from '@/data/works.json';
 
 export default () => {
   const [tab, setTab] = useState(0);
+  /* @ts-ignore */
+  const { t } = useTranslation('translation');
 
   const setClassName = (index: number) =>
     clsx({
-      'px-8 py-3 text-slate tracking-widest text-sm text-left border-l-2 border-neutro-dark font-thin':
+      'px-9 py-5 text-slate tracking-widest text-sm text-left border-l-2 border-neutro-dark font-thin':
         true,
       'hover:bg-navy-light': index !== tab,
       '!text-neon-green ease-in duration-150 !border-neon-green': index === tab,
     });
 
   return (
-    <div className="flex mt-10 space-x-2">
+    <div className="flex mt-10 space-x-2 w-11/12">
       <div className="flex flex-col">
         <Each
           of={works}
@@ -29,7 +32,7 @@ export default () => {
           )}
         />
       </div>
-      <div className="min-w-[500px] min-h-[400px] space-y-4">
+      <div className="w-3/4 min-h-[400px] space-y-8">
         <div className="space-y">
           <h3 className="text-xl font-bold text-white-light tracking-wider">
             {works[tab].position}
@@ -38,12 +41,12 @@ export default () => {
             {works[tab].dates}
           </p>
         </div>
-        <div className="w-4/5 space-x-1">
+        <div className="w-4/5 space-y-2">
           <Each
             of={works[tab].items}
             render={(item) => (
               <li className="flex font-thin items-center justify-start text-white-dark before:text-neon-green before:content-['▹'] before:text-[8px] before:m-3">
-                {item}
+                {t(item)}
               </li>
             )}
           />
