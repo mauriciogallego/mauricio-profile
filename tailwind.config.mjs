43c5891b8bf5f8252808409653c37cd7f9c87e0b
mauricio gallego <mgallego@sysgarage.com>
2024-05-13 19:03:07 -0500

    animation set

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
@@ -17,5 +20,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animated')],
 };
