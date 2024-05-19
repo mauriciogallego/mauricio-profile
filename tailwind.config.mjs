2cd5574b09fe4411066fb9be80fca2e7c44a50a1
mauricio gallego <mgallego@sysgarage.com>
2024-05-19 11:42:57 -0500

    improving nav bar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -15,6 +15,7 @@ export default {
       },
       boxShadow: {
         large: '0px 0px 65px rgba(0, 0, 0, 0.22)', // dark shadow
+        left: '-2px 0px 0.5px 0px rgba(0, 0, 0, 0.22)',
       },
     },
   },
